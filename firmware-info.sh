26794582b6951b7f72125c1e9602dab5e2379804
miruku <100756250+miruku1214@users.noreply.github.com>
2023-06-17 20:03:51 +0900

    Add files via upload

---
+++ firmware-info.sh
@@ -0,0 +1,15 @@
+echo 'Model:'
+getprop ro.product.odm.model
+echo
+echo 'Region:'
+getprop ro.csc.omcnw_code
+echo
+echo
+echo 'PDA:'
+getprop ro.build.PDA
+echo
+echo 'CSC:'
+getprop ril.official_cscver
+echo
+echo 'Phone:'
+getprop ro.build.PDA
